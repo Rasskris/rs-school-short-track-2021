5c9e35f398235c9f59762c542446a0199706d49d
Rasskris <rassoshenko.kristina@yandex.ru>
2021-04-12 21:02:35 +0300

    feat: add solution to 07 task

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,25 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const copy = names.slice();
+  const result = names.slice(0, 1);
+  const equalNames = {};
+  for (let i = 1; i < copy.length; i += 1) {
+    let k = 1;
+    const item = copy[i];
+    if (copy.slice(0, i).includes(item)) {
+      const num = equalNames[item] ? equalNames[item].k : k;
+      const renamed = `${item}(${num})`;
+      result.push(renamed);
+      copy.splice(i, 1, renamed);
+      k++;
+      equalNames[item] = { k };
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
 }
 
 module.exports = renameFiles;
